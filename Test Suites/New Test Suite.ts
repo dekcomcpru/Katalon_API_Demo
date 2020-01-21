ae52f9eb20cc2325f6854dfdc5f025c327a2cb0f
Extosoft <Extosoft@DESKTOP-COUMP7V>
2020-01-21 17:02:28 +0700

    first commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>78af03f1-406e-4050-8849-b29e8d583e92</testSuiteGuid>
+   <testCaseLink>
+      <guid>1f0c03a7-e68f-4365-9f64-778aaa547721</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
